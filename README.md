ab5ef52aa8c65271e31c5336b5bbe3007ee85bf9
Tate Capra <tatecapra@outlook.com>
2023-01-30 13:47:16 -0500

    Current version of app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TestLimeGreen
-Creating the base app for Lime Green without react. 
+
+Hello! My name is Tate.
+<br>
+Inprogress App: Web App for an upcoming business. Creating the base app for Lime Green without react. 
+
+Contact Info: Profile Page: https://spacemantxc.github.io/Profile/ Linkedin: www.linkedin.com/in/tate-capra
---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+//Main App
+
+//To Do:
+//Create a function at the load of the page that causes the page to
+// split in the middle where the Lime will be. Onload
+body.onload = function startPage(){
+    console.log("Test for start of page");
+}
+//Text Display Coming Soon Information
+
+//Home button to reload the page
+
+//Menu button that displays items that wil be on the menu
+let menuItems =["Fruit", "Berry", "LemonLime"];
